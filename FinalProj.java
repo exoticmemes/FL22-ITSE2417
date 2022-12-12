ddd73877a845e181fde61b3a9a0bf18352378bda
exoticmemes <72815680+exoticmemes@users.noreply.github.com>
2022-12-12 13:23:58 -0600

    Update FinalProj.java

--- FinalProj.java
+++ FinalProj.java
@@ -10,7 +10,12 @@ public class FinalProj implements FinalProject
 
     public void begin()
     {
-        System.out.println();
+        String[] signs = {"Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig"};
+        System.out.println("Welcome to the Chinese zodiac calculator!");
+        System.out.println("Enter your birth year: ");
+        int yearBorn = Itse2417Main.myScan.nextInt();
+        System.out.printf("You were born in the Year of the %s", signs[yearBorn % 12]);
+        /*System.out.println();
         System.out.println("Not implemented at this time");
         System.out.println();
         try
@@ -18,6 +23,6 @@ public class FinalProj implements FinalProject
             java.util.concurrent.TimeUnit.SECONDS.sleep(2);  
         }
         catch(InterruptedException e)
-        {}
+        {}*/
     }
 }
